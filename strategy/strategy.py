5b50911ab63fa39d538638598408a40ec56944f1
prog529007 <abhishek29007@gmail.com>
2024-09-22 15:38:34 -0500

    hmm

--- strategy/strategy.py
+++ strategy/strategy.py
@@ -44,10 +44,19 @@ class Strategy(BaseStrategy):
                     steer, second = plane_find_path_to_point(closest_enemy.position, plane)
                     response[id] = steer
                 else:
-                    response[id] = 0  # not sure
+                    response[id] = 0  # go straight
                     
             response[id] = min(1, response[id])
             response[id] = max(-1, response[id])
+            
+            if plane.position.x < -40 and self.team == 1:
+                response[id] = -1
+            elif plane.position.x < -40:
+                response[id] = 1
+            elif plane.position.x > 40 and self.team == 1:
+                response[id] = 1
+            elif plane.position.x > 40:
+                response[id] = -1
 
         return response
 
